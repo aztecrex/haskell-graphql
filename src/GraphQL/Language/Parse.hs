48fa0f18df735b43b27defe1b205e0c3436cfc04
Greg Wiley <gwiley@cj.com>
2018-08-31 15:23:12 -0700

    package refactor

---
+++ src/GraphQL/Language/Parse.hs
@@ -0,0 +1,101 @@
+module GraphQL.Language.Parse where
+
+import Control.Applicative ((<|>), many)
+import Control.Monad (when)
+import Data.Attoparsec.Text
+import Data.Char (isSpace, isDigit)
+import Data.Int (Int32)
+import Data.List.NonEmpty
+import Data.Monoid ((<>))
+import Data.Text (Text, append)
+import GraphQL.Language.Syntax
+
+
+-----------------------
+-- parse
+
+
+-- goals
+input1 :: Text
+input1 = "{ amount }"
+
+input2 :: Text
+input2 = "query myQuery { amount }"
+
+
+input3 :: Text
+input3 = "query { amount currency }"
+
+input4 :: Text
+input4 = "{ amount currency,posting_date }"
+
+
+output :: Document
+output = Document $
+            (ExecutableDefinition
+                (OpDefExecutableDefinition (
+                    SelSetOperationDefinition
+                        (SelectionSet [
+                            Field Nothing "amount" Nothing Nothing Nothing
+                        ])
+                    )
+                )
+            )
+            :| []
+
+
+document :: Parser Document
+document = Document <$> (ignored *>token definitions)
+
+definitions :: Parser (NonEmpty Definition)
+definitions = (:|) <$> definition <*> pure []
+
+definition :: Parser Definition
+definition = ExecutableDefinition <$> executableDefinition
+
+executableDefinition :: Parser ExecDef
+executableDefinition = OpDefExecutableDefinition <$> opDef
+
+opDef :: Parser OpDef
+opDef =
+            SelSetOperationDefinition <$> selSetOpDef
+        <|> OpTypeOperationDefinition <$> token opType <*> token (option Nothing (Just <$> name) ) <*> pure Nothing <*> pure Nothing <*> selSetOpDef
+
+opType :: Parser OperationType
+opType = QUERY <$ "query" <|> MUTATION <$ "mutation" <|> SUBSCRIPTION <$ "subscription"
+
+selSetOpDef :: Parser SelectionSet
+selSetOpDef = SelectionSet <$> (token "{" *> token selections <* token "}")
+
+selections :: Parser [Selection]
+selections = (:) <$> selection <*> many selection
+
+selection :: Parser Selection
+selection = Field <$> pure Nothing <*> token name <*> pure Nothing <*> pure Nothing <*> pure Nothing
+
+name :: Parser Text
+name = token $ append <$> takeWhile1 isA_z
+                    <*> Data.Attoparsec.Text.takeWhile ((||) <$> isDigit <*> isA_z)
+  where
+    isA_z =  inClass $ '_' : ['A'..'Z'] <> ['a'..'z']
+
+
+token :: Parser a -> Parser a
+token t = t <* (ignored <|> endOfInput)
+
+
+ignored :: Parser ()
+ignored =
+    do
+        c <- peekChar'
+        if (isSpace c || c == ',') -- this should be OK, spec has weird def of newline
+            then anyChar *> ignored
+            else when (c == '#') $ manyTill anyChar endOfLine *> ignored
+
+
+x :: Text
+x = ""
+
+p :: Parser Char
+p = ignored *> anyChar
+
